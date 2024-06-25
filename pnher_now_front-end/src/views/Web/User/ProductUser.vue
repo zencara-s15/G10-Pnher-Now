7333660880dbc7b6c5b4d04a937b11b06dc58503
CHA EM <cha.em@student.passerellesnumeriques.org>
2024-06-25 10:51:25 +0700

    commit to save code

--- pnher_now_front-end/src/views/Web/User/ProductUser.vue
+++ pnher_now_front-end/src/views/Web/User/ProductUser.vue
@@ -1,9 +1,9 @@
 <template>
   <WebLayout></WebLayout>
-  <div class="container mt-4 bg-dark p-4">
+  <div class="container mt-4 p-4">
     <div class="row justify-content-between align-items-center mb-5">
       <div class="col-auto">
-        <h1 class="fw-bolder text-white">List of User Product</h1>
+        <h3 class="fw-bolder text-dark">List of User Product</h3>
       </div>
       <div class="col-auto">
         <div class="input-group search-group mr-50">
@@ -25,12 +25,16 @@
     <div class="row">
       <div class="col-md-4 mb-3" v-for="(item, index) in user_create" :key="index">
         <div class="card">
+          <div class="d-flex justify-content-center mt-2">
+            <h3>Product</h3>
+          </div>
           <div class="card-body">
-            <h5 class="card-title">Sender: {{ item.sender }}</h5>
+            <p class="card-title">Sender: {{ item.sender }}</p>
             <p class="card-text">Receiver: {{ item.receiver }}</p>
             <p class="card-text">From: {{ item.from }}</p>
             <p class="card-text">To: {{ item.to }}</p>
-            <p class="card-text">Category: {{ item.category }}</p>
+            <p class="card-text">Product Type: {{ item.product_type }}</p>
+            <p class="card-text">Company: {{ item.company }}</p>
           </div>
         </div>
       </div>
@@ -40,9 +44,9 @@
   <!-- Form -->
   <div v-if="showModal" class="modal fade show" style="display: block" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
-      <div class="modal-content bg-orange">
+      <div class="modal-content mt-20">
         <div class="modal-header d-flex justify-content-between align-items-center">
-          <h5 class="modal-title">Add Your Product</h5>
+          <h5 class="modal-title text-white">Add Your Product</h5>
           <button
             type="button"
             class="close btn bg-danger"
@@ -55,16 +59,16 @@
         <div class="modal-body">
           <form @submit.prevent="addItem">
             <div class="form-group">
-              <label for="sender">Sender</label>
+              <label for="sender" class="text-danger">Phone Sender</label>
               <input type="text" v-model="sender" class="form-control" id="sender" required />
             </div>
             <div class="form-group">
-              <label for="receiver">Receiver</label>
+              <label for="receiver" class="text-danger">Phone Receiver</label>
               <input type="text" v-model="receiver" class="form-control" id="receiver" required />
             </div>
             <div>
               <div class="form-group">
-                <label for="from">From</label>
+                <label for="from" class="text-danger">From</label>
                 <select v-model="from" class="form-control" id="from" required>
                   <option value="" disabled>Select your location</option>
                   <option v-for="province in provinces" :key="province" :value="province">
@@ -73,7 +77,7 @@
                 </select>
               </div>
               <div class="form-group">
-                <label for="to">To</label>
+                <label for="to" class="text-danger">To</label>
                 <select v-model="to" class="form-control" id="to" required>
                   <option value="" disabled>Select your goal</option>
                   <option v-for="province in provinces" :key="province" :value="province">
@@ -83,22 +87,32 @@
               </div>
             </div>
             <div class="form-group">
-              <label for="category">Category</label>
-              <select v-model="category" class="form-control" id="category" required>
-                <option value="" disabled>Select category</option>
-                <option v-for="category in categories" :key="category" :value="category">
-                  {{ category }}
+              <label for="product_type" class="text-danger">Product Type</label>
+              <select v-model="product_type" class="form-control" id="product_type" required>
+                <option value="" disabled>Select type of Product</option>
+                <option
+                  v-for="product_type in product_types"
+                  :key="product_type"
+                  :value="product_type"
+                >
+                  {{ product_type }}
+                </option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="company" class="text-danger">Choose Company</label>
+              <select v-model="company" class="form-control" id="company" required>
+                <option value="" disabled>Select Company</option>
+                <option v-for="company in companies" :key="company" :value="company">
+                  {{ company }}
                 </option>
               </select>
             </div>
             <div>
-              <div class="flex gap-30">
-                <button type="submit" class="btn btn-primary mt-4">Pay By Sender</button>
-                <button type="submit" class="btn btn-primary mt-4">Pay BY Receiver</button>
+              <div class="flex gap-53">
+                <button type="submit" class="btn bg-white mt-4">Pay By Sender</button>
+                <button type="submit" class="btn bg-white mt-4">Pay BY Receiver</button>
               </div>
-            </div>
-            <div>
-              <button type="submit" class="btn bg-white mt-4">Choose Company</button>
             </div>
           </form>
         </div>
@@ -121,23 +135,29 @@ export default {
       receiver: '',
       from: '',
       to: '',
-      category: '',
+      product_type: '',
+      company: '',
 
       user_create: [
         {
           sender: '088739393',
           receiver: '088343434',
           from: 'PP',
-          to: 'Kampot'
+          to: 'Kampot',
+          product_type: 'decaying',
+          company: 'Vireak Buntham'
         },
         {
           sender: '088739393',
           receiver: '088343434',
           from: 'PP',
-          to: 'Takaw'
+          to: 'Takeaw',
+          product_type: 'not decaying',
+          company: 'Vireak Buntham'
         }
       ],
-      categories: ['food', 'drink', 'electronics', 'clothing', 'other'],
+      product_types: ['decaying', 'not decaying'],
+      companies: ['Vireak Buntham', 'Capitol', 'J&T express', 'ZTO express', 'other'],
       from: '',
       to: '',
       provinces: [
@@ -177,13 +197,15 @@ export default {
         receiver: this.receiver,
         from: this.from,
         to: this.to,
-        category: this.category
+        product_type: this.product_type,
+        company: this.company
       })
       this.sender = ''
       this.receiver = ''
       this.from = ''
       this.to = ''
-      this.category = ''
+      this.product_type = ''
+      this.company = ''
       this.showModal = false
     }
   }
@@ -213,5 +235,8 @@ export default {
 
 .modal.show .modal-dialog {
   transform: translate(0, 0);
+}
+.modal-content {
+  background: #2a2726;
 }
 </style>
