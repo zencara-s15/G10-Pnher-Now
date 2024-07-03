dc483fa68433534c85d7b3502cd0288f88df7703
CHA EM <cha.em@student.passerellesnumeriques.org>
2024-07-03 08:15:32 +0700

    [imp]improve code that have conflict

--- pnher_now_back-end/routes/web.php
+++ pnher_now_back-end/routes/web.php
@@ -59,6 +59,7 @@ Route::namespace('App\Http\Controllers\Admin')->name('admin.')->prefix('admin')
         Route::resource('permissions','PermissionController');
         Route::resource('users','UserController');
         Route::resource('posts','PostController');
+        Route::resource('supervisor','SupervisorController');
 
         Route::get('/profile',[ProfileController::class,'index'])->name('profile');
         Route::put('/profile-update',[ProfileController::class,'update'])->name('profile.update');
