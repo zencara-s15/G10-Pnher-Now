5dc51c8ad4e21fca8954fd453800d1f367e6b522
BUNNARITHHHHH <bunnarith.phoeurn@student.passerellesnumeriques.org>
2024-07-04 16:28:53 +0700

    [imp] fix conflict

--- pnher_now_back-end/resources/views/layouts/app.blade.php
+++ pnher_now_back-end/resources/views/layouts/app.blade.php
@@ -19,50 +19,49 @@
     <!-- Material Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
 
+    <!-- Other head elements -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-    <div class="flex-1 flex flex-col overflow-scroll">
 
-        <!-- Other head elements -->
-        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-        <!-- Chart JS -->
-        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <!-- Chart JS -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 
 <body class="font-sans antialiased">
     <div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200">
-        <div :class="sidebarOpen ? 'block' : 'hidden'" @click="sidebarOpen = false" class="fixed z-20 inset-0 bg-black opacity-50 transition-opacity lg:hidden"></div>
+        <div :class="sidebarOpen ? 'block' : 'hidden'" @click="sidebarOpen = false"
+            class="fixed z-20 inset-0 bg-black opacity-50 transition-opacity lg:hidden"></div>
 
         @include('layouts.sidebar')
 
-        <div class="flex-1 flex flex-col overflow-hidden">
+        <div class="flex-1 flex flex-col overflow-scroll">
 
             @include('layouts.header')
 
-            @if(\Session::has('success'))
-            <div class="text-green-600 pt-5 pl-5">
-                <ul>
-                    <li>{!! \Session::get('success') !!}</li>
-                </ul>
-            </div>
+            @if (\Session::has('success'))
+                <div class="text-green-600 pt-5 pl-5">
+                    <ul>
+                        <li>{!! \Session::get('success') !!}</li>
+                    </ul>
+                </div>
             @endif
 
-            @if(\Session::has('error'))
-            <div class="text-green-600 pt-5 pl-5">
-                <ul>
-                    <li>{!! \Session::get('error') !!}</li>
-                </ul>
-            </div>
+            @if (\Session::has('error'))
+                <div class="text-green-600 pt-5 pl-5">
+                    <ul>
+                        <li>{!! \Session::get('error') !!}</li>
+                    </ul>
+                </div>
             @endif
 
             @if ($errors->any())
-            <div class="text-red-600  pt-5 pl-5">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
+                <div class="text-red-600  pt-5 pl-5">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
             @endif
 
             {{ $slot }}
