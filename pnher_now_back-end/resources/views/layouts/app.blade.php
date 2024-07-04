2a3d8495f006a7cda1d426e09b58bf8cc09104ec
BUNNARITHHHHH <bunnarith.phoeurn@student.passerellesnumeriques.org>
2024-07-04 16:21:07 +0700

    [imp] fix conflict

--- pnher_now_back-end/resources/views/layouts/app.blade.php
+++ pnher_now_back-end/resources/views/layouts/app.blade.php
@@ -7,56 +7,62 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/3.10.3/cdn.min.js" integrity="sha512-6LOuSt6loBXwUs8BGpCe7XsllXZBP4EpFE3mZ5Ibax0C7hBymqj0KzDH+brgr07Yr++NTOh3aIv2+8KvEvNOcA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
 
     <!-- Scripts -->
-    <!-- Chart JS -->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Material Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
+
+
+    <div class="flex-1 flex flex-col overflow-scroll">
+
+        <!-- Other head elements -->
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+        <!-- Chart JS -->
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 
 <body class="font-sans antialiased">
     <div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200">
-        <div :class="sidebarOpen ? 'block' : 'hidden'" @click="sidebarOpen = false"
-            class="fixed z-20 inset-0 bg-black opacity-50 transition-opacity lg:hidden"></div>
+        <div :class="sidebarOpen ? 'block' : 'hidden'" @click="sidebarOpen = false" class="fixed z-20 inset-0 bg-black opacity-50 transition-opacity lg:hidden"></div>
 
         @include('layouts.sidebar')
 
-        <div class="flex-1 flex flex-col overflow-scroll">
+        <div class="flex-1 flex flex-col overflow-hidden">
 
             @include('layouts.header')
 
-            @if (\Session::has('success'))
-                <div class="text-green-600 pt-5 pl-5">
-                    <ul>
-                        <li>{!! \Session::get('success') !!}</li>
-                    </ul>
-                </div>
+            @if(\Session::has('success'))
+            <div class="text-green-600 pt-5 pl-5">
+                <ul>
+                    <li>{!! \Session::get('success') !!}</li>
+                </ul>
+            </div>
             @endif
 
-            @if (\Session::has('error'))
-                <div class="text-green-600 pt-5 pl-5">
-                    <ul>
-                        <li>{!! \Session::get('error') !!}</li>
-                    </ul>
-                </div>
+            @if(\Session::has('error'))
+            <div class="text-green-600 pt-5 pl-5">
+                <ul>
+                    <li>{!! \Session::get('error') !!}</li>
+                </ul>
+            </div>
             @endif
 
             @if ($errors->any())
-                <div class="text-red-600  pt-5 pl-5">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
+            <div class="text-red-600  pt-5 pl-5">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
             @endif
 
             {{ $slot }}
