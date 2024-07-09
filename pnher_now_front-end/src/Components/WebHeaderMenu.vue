81aeafb6ecdfc01cd16d2c45f53d8e7309e00c95
sreyluchly <sreyluch.ly@student.passerellesnumeriques.org>
2024-07-09 15:19:55 +0700

    [fear] create home page

--- pnher_now_front-end/src/Components/WebHeaderMenu.vue
+++ pnher_now_front-end/src/Components/WebHeaderMenu.vue
@@ -15,7 +15,7 @@ function closeDropdown() {
 
 <template>
   <nav
-    class="flex-no-wrap flex w-full items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:flex-wrap lg:justify-start lg:py-4 size-25"
+    class="flex-no-wrap relative flex w-full items-center justify-between bg-gray-500 py-2 shadow-dark-mild dark:bg-neutral-700 lg:flex-wrap lg:justify-start lg:py-4 size-25"
   >
     <div class="flex w-full flex-wrap items-center justify-between px-3">
       <!-- Logo -->
@@ -23,7 +23,7 @@ function closeDropdown() {
         to="/"
         class="flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:mb-0 lg:mt-0"
       >
-        <img src="../assets/logo.png" style="height: 50px" alt="TE Logo" loading="lazy" />
+        <img src="../assets/Pnher now LOGO.png" style="height: 50px" alt="TE Logo" loading="lazy" />
       </router-link>
       <!-- Left navigation links -->
       <div class="flex-grow">
--- pnher_now_front-end/src/views/Web/HomeView.vue
+++ pnher_now_front-end/src/views/Web/HomeView.vue
@@ -1,10 +1,10 @@
-<script setup lang="ts" >
-import WebLayout from '@/Components/Layouts/WebLayout.vue'
-// import HeaderHome from '@/Components/HeaderUser/HeaderHome.vue'
-</script>
-
 <template>
-  <WebLayout>
-  </WebLayout>
-  <!-- <HeaderHome></HeaderHome> -->
+  <NavHome></NavHome>
+  <HeaderHome></HeaderHome>
+  <Footers></Footers>
 </template>
+<script setup lang="ts">
+  import NavHome from '@/Components/NavHome.vue'
+  import HeaderHome from '@/Components/HeaderUser/HeaderHome.vue'
+  import Footers from '@/Components/Footers/Footer.vue'
+</script>
