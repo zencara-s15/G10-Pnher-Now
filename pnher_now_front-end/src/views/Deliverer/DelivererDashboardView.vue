f90bc66fd6ffa1d828c8e5fbc5adcf2e2a873762
CHA EM <cha.em@student.passerellesnumeriques.org>
2024-07-15 20:02:38 +0700

    [feat] post and pickup baggage

--- pnher_now_front-end/src/views/Deliverer/DelivererDashboardView.vue
+++ pnher_now_front-end/src/views/Deliverer/DelivererDashboardView.vue
@@ -1,10 +1,304 @@
-<script setup lang="ts">
-// import AdminLayout from '@/Components/Layouts/AdminLayout.vue'
-import DelivererLayout from '@/Components/Layouts/DelivererLayout.vue'
-
-</script>
 <template>
   <DelivererLayout>
-    <h1>Here my Deliverer Dashboard</h1>
+    <div class="delivers mt-5">
+      <nav
+        class="form-delivers delivers-expand-lg flex-row"
+        v-for="(item, index) in baggage"
+        :key="item.id"
+      >
+        <div class="card" v-if="item.delivery_status_id === 1">
+          <div class="card-body" v-if="!hideCardBody">
+            <div class="card-header">
+              <div class="img">
+                <img :src="`http://127.0.0.1:8000/images/${item.post_id?.profile}`" />
+              </div>
+              <div class="card-title">
+                <p class="name">
+                  Name: {{ users[index]?.first_name + ' ' + users[index]?.last_name }}
+                </p>
+              </div>
+            </div>
+            <div class="card-actions">
+              <button type="button" class="btn btn-info" @click="showPostDetails(item)">
+                Detail
+              </button>
+              <!-- <button type="button" class="btn btn-warning" @click="toggleCardBody">Cancel</button> -->
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div v-if="isAlertVisible" class="alert-card">
+        <div class="alert-card-body card-deliverer mb-5">
+          <div class="information">
+            <div class="text-white d-flex justify-content-center p-2 bg-red radius1">
+              <h2>Information</h2>
+            </div>
+            <div class="p-3">
+              <p>Phone Receiver: {{ selectedBaggage.phone_receiver }}</p>
+              <p>Sending Address: {{ selectedBaggage.sending_address }}</p>
+              <p>Receiving Address: {{ selectedBaggage.receiving_address }}</p>
+              <p>Baggage Type: {{ selectedBaggage.type }}</p>
+              <p>Weight: {{ selectedBaggage.weight }} Kg</p>
+              <p>Total Cost: {{ calculateTotalCost(selectedBaggage.weight) }} Real</p>
+              <p>Company: {{ selectedBaggage.company }}</p>
+            </div>
+            <div class="d-flex justify-center image">
+              <img src="/src/assets/deliverer.png" width="300px" />
+            </div>
+            <div class="mt-3 p-3 d-flex justify-between">
+              <div>
+                <button type="button" class="btn btn-outline-danger" @click="closeAlert">
+                  Back
+                </button>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  class="btn btn-success"
+                  @click="acceptDelivery(selectedBaggage, index)"
+                >
+                  Accept
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="Map">
+            <div class="text-white d-flex justify-content-center p-2 radius2">
+              <h2>Map</h2>
+            </div>
+            <div class="google-map">
+              <!-- Map component goes here -->
+              <GoogleMap />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </DelivererLayout>
 </template>
+
+<script>
+import DelivererLayout from '@/Components/Layouts/DelivererLayout.vue'
+import { ref, onMounted } from 'vue'
+import { usePostBaggageStore } from '@/stores/post_baggage-list'
+import GoogleMap from '@/Components/Layouts/GoogleMap.vue'
+
+export default {
+  components: {
+    DelivererLayout,
+    GoogleMap
+  },
+  name: 'Deliverer',
+  setup() {
+    const baggageStore = usePostBaggageStore()
+    const baggage = ref([])
+    const users = ref([])
+
+    onMounted(async () => {
+      await baggageStore.fetchPostBaggage()
+      users.value = baggageStore.user_baggage
+      baggage.value = baggageStore.post_baggage
+      console.log(baggage)
+    })
+
+    const calculateTotalCost = (weight) => {
+      // Replace with your actual logic to calculate total cost based on weight
+      return weight * 6000 // Assuming a rate of 6000 Real per Kg
+    }
+
+    return {
+      baggage,
+      users,
+      calculateTotalCost
+    }
+  },
+  data() {
+    return {
+      senderDetail: {
+        name: '',
+        items: '',
+        location: '',
+        phone_number: '',
+        map: ''
+      },
+      selectedBaggage: ref(null),
+      isAlertVisible: false,
+      hideCardBody: false,
+      delivery_status_id: ref(null)
+    }
+  },
+  computed: {
+    senderId() {
+      const urlParams = new URLSearchParams(window.location.search)
+      return urlParams.get('sender') || Math.floor(Math.random() * 10) + 1
+    }
+  },
+  methods: {
+    async fetchSenderDetails() {},
+    async showPostDetails(baggage) {
+      this.selectedBaggage = baggage
+      console.log(baggage)
+      this.isAlertVisible = true
+      this.hideCardBody = false
+
+      this.baggageStore = usePostBaggageStore()
+      this.delivery_status_id = '1'
+    },
+    async acceptDelivery(baggage, index) {
+      this.selectedBaggage = baggage
+      this.isAlertVisible = true
+      this.hideCardBody = false
+      console.log(baggage)
+      console.log(index)
+
+      this.baggageStore = usePostBaggageStore()
+      this.delivery_status_id = '2'
+      try {
+        await this.baggageStore.updateBaggageStatus(this.delivery_status_id, baggage.id)
+        window.location.href = '/deliverer'
+        console.log(this.baggageStore.responseMessage)
+      } catch (error) {
+        console.warn(error)
+      }
+    },
+    async acceptBaggage() {
+      this.delivery_status_id = '3'
+      try {
+        await this.baggageStore.updateBaggageStatus(
+          this.delivery_status_id,
+          this.selectedBaggage.id
+        )
+        this.closeAlert()
+        console.log(this.baggageStore.responseMessage)
+      } catch (error) {
+        console.warn(error)
+      }
+    },
+    toggleCardBody() {
+      this.hideCardBody = !this.hideCardBody
+    },
+    closeAlert() {
+      this.isAlertVisible = false
+    }
+  },
+  created() {
+    this.fetchSenderDetails()
+  }
+}
+</script>
+
+<style scoped>
+.delivers {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+.card {
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  padding: 20px;
+  margin-bottom: 15px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  width: 250px; /* Set width and height to make the card square */
+  height: 250px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.card-body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  height: 100%;
+  justify-content: space-between;
+}
+
+.card-header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.card-header img {
+  width: 80px;
+  height: 80px;
+  border-radius: 10px;
+  margin-bottom: 10px;
+}
+
+.card-title {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.card-content {
+  margin-bottom: 10px;
+}
+
+.card-actions {
+  display: flex;
+  justify-content: center;
+  gap: 30px;
+}
+
+.alert-card {
+  display: flex;
+  align-content: center;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 20px;
+  width: 100%;
+  background: #000000;
+  height: 100%;
+}
+
+.alert-card-body {
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  box-shadow: 0 4px 8px rgb(6, 1, 1);
+  background-color: white;
+  border: 1px solid #ccc;
+  text-align: left;
+  width: 100%;
+}
+
+.alert-card h5 {
+  margin-bottom: 10px;
+}
+
+.card-deliverer {
+  display: flex;
+  flex-direction: row;
+  width: 80%;
+  height: 70vh;
+  border-radius: 10px 10px 10px;
+}
+.information,
+.Map {
+  width: 100%;
+  height: 100%;
+}
+.radius1 {
+  background: #d23202;
+  border-radius: 10px 0px 0px 0px;
+}
+.radius2 {
+  background: #d23202;
+  border-radius: 0px 10px 0px 0px;
+}
+.image {
+  width: 450px;
+  height: 300px;
+  margin-bottom: -90px;
+}
+</style>
