ae026e2da6df0d38199a4bbfd81b28dd641f1612
CHA EM <cha.em@student.passerellesnumeriques.org>
2024-07-15 20:06:10 +0700

    [feat] Already post and pick up baggages

--- pnher_now_back-end/app/Http/Resources/BaggageResource.php
+++ pnher_now_back-end/app/Http/Resources/BaggageResource.php
@@ -16,15 +16,15 @@ class BaggageResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'name' => $this->name,  // Assuming `name` is an attribute of `Baggage`
-            'user' => [
-                'id' => $this->user->id,
-                'name' => $this->user->name,
-            ],
-            'deliveryStatus' => [
-                'id' => $this->deliveryStatus->id,
-                'name' => $this->deliveryStatus->name,
-            ],
+            'phone_receiver' => $this->receiver_phone,
+            'sending_address'=>$this->sending_address,
+            'receiving_address'=>$this->receiving_address,
+            'type'=>$this->type,
+            'weight'=>$this->weight,
+            'company'=>$this->company,
+            'delivery_status_id'=>$this->delivery_status_id,
+            'post_id'=>$this->user  // Assuming `name` is an attribute of `Baggage`
+            
         ];
     }
 }
